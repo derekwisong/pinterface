e9789670a1798b67e16206a9a98cae65e41eacd7
Derek Wisong <derekwisong@gmail.com>
2021-04-26 16:04:24 +0100

    Update colors on knob turn

--- pinterface/demo/simple.py
+++ pinterface/demo/simple.py
@@ -5,11 +5,17 @@ import board
 import digitalio
 
 from adafruit_rgb_display.st7735 import ST7735R
+from adafruit_rgb_display import color565
 from gpiozero import RotaryEncoder, Button
 
 from tenacity import retry
 from tenacity.stop import stop_after_delay
 
+color = {"Red": 0, "Green": 0, "Blue": 0}
+
+def get_color():
+    rgb = (color["Red"], color["Green"], color["Blue"])
+    return color565(*rgb)
 
 @retry(stop=stop_after_delay(10))
 def retry_call(callable: Callable, *args, **kwargs):
@@ -17,10 +23,13 @@ def retry_call(callable: Callable, *args, **kwargs):
     return callable(*args, **kwargs)
 
 
-def twist_knob(knob: RotaryEncoder, label):
-    # convert the knobs step counter into a 0-255 value
+def twist_knob(event: Event, knob: RotaryEncoder, label):
+    global color
     val = min(knob.steps + 128, 255)
     print(f"Knob {label} steps={knob.steps} value={knob.value} val={val}")
+    color[label] = val
+    # trigger color change
+    event.set()
 
 
 def press_knob(label):
@@ -38,7 +47,8 @@ def press_button(label):
 
 if __name__ == "__main__":
     print(f"Initializing inputs and outputs")
-    event = Event()
+    stop_event = Event()
+    color_event = Event()
 
     # knobs will track a step counter from (-128, 128)
     # twisting clockwise increments counter, counter-clockwise decrements
@@ -58,30 +68,18 @@ if __name__ == "__main__":
     button_d = retry_call(Button, 3)   # rewind
     button_e = retry_call(Button, 26)  # ffwd
 
-    display = ST7735R(board.SPI(),
-                      rotation=90,
-                      width=128,
-                      height=160,
-                      cs=digitalio.DigitalInOut(board.CE0),
-                      dc=digitalio.DigitalInOut(board.D24),
-                      rst=digitalio.DigitalInOut(board.D25),
-                      baudrate=40000000)
-
-    print(f"Setting display to black")
-    display.fill(0)
-
     # add knob callbacks
-    knob_a.when_rotated = lambda x: twist_knob(knob_a, "A")
-    knob_b.when_rotated = lambda x: twist_knob(knob_b, "B")
-    knob_c.when_rotated = lambda x: twist_knob(knob_c, "C")
+    knob_a.when_rotated = lambda x: twist_knob(color_event, knob_a, "Red")
+    knob_b.when_rotated = lambda x: twist_knob(color_event, knob_b, "Green")
+    knob_c.when_rotated = lambda x: twist_knob(color_event, knob_c, "Blue")
 
     # can implement other knob callbacks...
     # knob_a.when_rotated_clockwise = fn()
     # knob_a.when_rotated_counter_clockwise = fn()
 
-    knob_a_button.when_released = lambda x: press_knob("A")
-    knob_b_button.when_released = lambda x: press_knob("B")
-    knob_c_button.when_released = lambda x: press_knob("C")
+    knob_a_button.when_released = lambda x: press_knob("Red")
+    knob_b_button.when_released = lambda x: press_knob("Green")
+    knob_c_button.when_released = lambda x: press_knob("Blue")
 
     # can also read values off buttons instead of callbacks if
     # working in a loop
@@ -89,9 +87,9 @@ if __name__ == "__main__":
     #     if knob_a_button.is_pressed:
     #         print("button A is pressed")
 
-    knob_a_button.when_held = lambda x: hold_knob(event, "A")
-    knob_b_button.when_held = lambda x: hold_knob(event, "B")
-    knob_c_button.when_held = lambda x: hold_knob(event, "C")
+    knob_a_button.when_held = lambda x: hold_knob(stop_event, "Red")
+    knob_b_button.when_held = lambda x: hold_knob(stop_event, "Green")
+    knob_c_button.when_held = lambda x: hold_knob(stop_event, "Blue")
 
     button_a.when_released = lambda x: press_button("A")
     button_b.when_released = lambda x: press_button("B")
@@ -99,12 +97,25 @@ if __name__ == "__main__":
     button_d.when_released = lambda x: press_button("D")
     button_e.when_released = lambda x: press_button("E")
 
+    print(f"Initializing display")
+    display = ST7735R(board.SPI(),
+                    rotation=90,
+                    width=128,
+                    height=160,
+                    cs=digitalio.DigitalInOut(board.CE0),
+                    dc=digitalio.DigitalInOut(board.D24),
+                    rst=digitalio.DigitalInOut(board.D25),
+                    baudrate=40000000)
+
+    display.fill(get_color())
+
     print("Twist and press the knobs and buttons.")
     print("Press a knob for longer than 3 seconds or Ctrl-C to exit.")
 
     try:
-        while not event.wait(timeout=0.5):
-            # would like to try updating the screen 
-            pass
+        while not stop_event.wait(timeout=0.001):
+            if color_event.is_set():
+                display.fill(get_color())
+                color_event.clear()
     except KeyboardInterrupt:
         exit(0)
